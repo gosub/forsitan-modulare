87fdc86f0774720470c2ce53fcd371bb2dcb224e
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2020-01-18 15:11:34 +0100

    renamed plugin.* to forsitan.*

--- src/forsitan.cpp
+++ src/forsitan.cpp
@@ -1,4 +1,4 @@
-#include "plugin.hpp"
+#include "forsitan.hpp"
 
 
 Plugin* pluginInstance;
@@ -8,7 +8,7 @@ void init(Plugin* p) {
 	pluginInstance = p;
 
 	// Add modules here
-	// p->addModel(modelMyModule);
+	p->addModel(alea);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
