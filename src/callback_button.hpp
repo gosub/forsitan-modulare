cc20f1f025b678afdd30403be3dec4c2b55cb2df
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2020-01-19 00:15:33 +0100

    alea now adds a single random module on button press

---
+++ src/callback_button.hpp
@@ -0,0 +1,37 @@
+#ifndef CALLBACK_BUTTON_HPP__
+#define CALLBACK_BUTTON_HPP__
+
+#include "plugin.hpp"
+#include "window.hpp"
+#include <functional>
+
+template<typename T>
+struct CallbackButton : SvgButton {
+  static CallbackButton* create(const Vec& v,
+				std::function<void (T*)> c,
+				T* m,
+				std::shared_ptr<rack::Svg> button_idle,
+				std::shared_ptr<rack::Svg> button_clicked) {
+    CallbackButton* cb = createWidget<CallbackButton<T> >(v);
+    cb->callback = c;
+    cb->module = m;
+    cb->addFrame(button_idle);
+    cb->addFrame(button_clicked);
+    cb->shadow->opacity = -1.f;
+    return cb;
+  }
+
+  CallbackButton() {}
+
+  virtual void onAction(const event::Action &e) override {
+    e.consume(this);
+    callback(module);
+    SvgButton::onAction(e);
+  }
+
+  std::function<void (T*)> callback;
+  T* module;
+};
+
+
+#endif
