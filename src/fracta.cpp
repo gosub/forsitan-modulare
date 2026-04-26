93cf2933eea9b0a73338612eb128ab3ed211f096
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2026-04-26 19:42:37 +0200

    fix(fracta): reduce title size and inset screws by 1HP
    Title cap height reduced from 4.6mm to 3.5mm to match other modules
    (interea ~3.86mm, alea/pavo ~3.38mm). Baseline raised to y=8.5mm.
    
    All four screws now inset one HP from each edge, matching interea layout.

--- src/fracta.cpp
+++ src/fracta.cpp
@@ -284,10 +284,10 @@ struct FractaWidget : ModuleWidget {
         setModule(module);
         setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/fracta.svg")));
 
-        addChild(createWidget<ScrewSilver>(Vec(0, 0)));
-        addChild(createWidget<ScrewSilver>(Vec(box.size.x - RACK_GRID_WIDTH, 0)));
-        addChild(createWidget<ScrewSilver>(Vec(0, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-        addChild(createWidget<ScrewSilver>(Vec(box.size.x - RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
         // CORE section
         addParam(createParamCentered<TL1105>(mm2px(Vec(12.0, 21.0)), module, Fracta::CORE_BTN_PARAM));
